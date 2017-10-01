3a244a97445b86c599062d48cfded880e89fe2cf
Haritha Rathinakumar <hrathin@g.clemson.edu>
2017-10-01 11:28:11 -0400

    Add project details to portfolio

--- js/main.js
+++ js/main.js
@@ -25,4 +25,17 @@ $(document).ready(function() {
         }
       }
   });
+  $("#projects-owl-slider").owlCarousel({
+      nav: true,
+      loop: true,
+      pagination: true,
+      responsive: {
+        0: {
+          items: 1
+        },
+        500: {
+          items: 3
+        }
+      }
+  });
 })

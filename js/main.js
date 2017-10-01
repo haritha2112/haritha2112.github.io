9c164ebe85d984cb68202de4a6f180f88067ee0c
Haritha Rathinakumar <hrathin@g.clemson.edu>
2017-10-02 01:28:03 +0530

    Use formspree to send emails from contact form

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+function initializeCarousels() {
   $("#work-exp-owl-slider").owlCarousel({
       nav: true,
       loop: true,
@@ -38,4 +38,46 @@ $(document).ready(function() {
         }
       }
   });
-})
+}
+
+function attatchCloseEventToAlertBoxes() {
+  $('.alert-box').on('click', '.close', function() {
+	  $(this).parent().fadeOut(500);
+	});
+}
+
+function initializeContactForm() {
+  const successAlert = `<div class="alert-box ss-success hideit">
+                          <p>Your message has been sent :)</p>
+                          <i class="fa fa-times close"></i>
+                        </div>`;
+  const errorAlert = `<div class="alert-box ss-error hideit">
+                        <p>Oops! Something went wrong :(</p>
+                        <i class="fa fa-times close"></i>
+                      </div>`;
+  $("#contactForm").submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: "https://formspree.io/hrathin@g.clemson.edu",
+      method: "POST",
+      data: $(this).serialize(),
+      dataType: "json",
+      beforeSend: function() {
+        $("#contact-form-messages").find('.alert-box').hide();
+      },
+      success: function(data) {
+        $("#contact-form-messages").append(successAlert);
+        attatchCloseEventToAlertBoxes();
+      },
+      error: function(err) {
+        $("#contact-form-messages").append(errorAlert);
+        attatchCloseEventToAlertBoxes();
+      }
+    });
+  });
+}
+
+$(document).ready(function() {
+  initializeCarousels();
+  initializeContactForm();
+});

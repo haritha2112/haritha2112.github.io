e493caeed5426f6f181a1e7c0b4d767a74017ad1
Haritha Rathinakumar <hrathin@g.clemson.edu>
2017-10-01 05:29:57 -0400

    Add skills using owl carousel

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("#owl-slider").owlCarousel({
+  $("#work-exp-owl-slider").owlCarousel({
       nav: true,
       loop: true,
       pagination: true,
@@ -9,6 +9,19 @@ $(document).ready(function() {
         },
         500: {
           items: 2
+        }
+      }
+  });
+  $("#skills-owl-slider").owlCarousel({
+      nav: true,
+      loop: true,
+      pagination: true,
+      responsive: {
+        0: {
+          items: 1
+        },
+        500: {
+          items: 3
         }
       }
   });

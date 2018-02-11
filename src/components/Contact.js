fcd65d3b61034c8c751717c3c0e407f061732379
haritha2112 <hrathin@g.clemson.edu>
2018-02-11 10:25:35 -0500

    Integrate Formspree in contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,35 +1,110 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 export default class Contact extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+      errorNotificationVisible: "",
+      successNotificationVisible: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleSubjectChange = this.handleSubjectChange.bind(this);
+    this.handleMessageChange = this.handleMessageChange.bind(this);
+    this.showSuccessMessage = this.showSuccessMessage.bind(this);
+    this.showErrorMessage = this.showErrorMessage.bind(this);
+  }
+
+  showSuccessMessage() {
+    this.setState({successNotificationVisible: "visible"});
+    setTimeout((() => {
+      this.setState({successNotificationVisible: ""});
+    }).bind(this), 5000);
+  }
+
+  showErrorMessage() {
+    this.setState({errorNotificationVisible: "visible"});
+    setTimeout((() => {
+      this.setState({errorNotificationVisible: ""});
+    }).bind(this), 5000);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const {name, email, subject, message} = this.state;
+    const data = {
+      name: name,
+      email: email,
+      subject: subject,
+      message: message,
+      _reply_to: email,
+      _subject: subject
+    };
+    axios.post("https://formspree.io/hrathin@g.clemson.edu", data).then((response) => {
+      this.showSuccessMessage();
+    }).catch((error) => {
+      this.showErrorMessage();
+    });
+  }
+
+  handleNameChange(event) {
+    this.setState({name: event.target.value});
+  }
+
+  handleEmailChange(event) {
+    this.setState({email: event.target.value});
+  }
+
+  handleSubjectChange(event) {
+    this.setState({subject: event.target.value});
+  }
+
+  handleMessageChange(event) {
+    this.setState({message: event.target.value});
+  }
+
   render() {
     return (
       <div className="main-content contact">
-        <div class="message">
+        <div className="message">
           I'd Love To Hear From You.
         </div>
-        <div class="contact-form">
-          <form name="contactForm" id="contactForm">
-            <div class="form-field">
+        <div className="notifications">
+          <div className={"notification success " + this.state.successNotificationVisible}>
+            Your message has been sent. I'll reach out to you shortly. ^_^
+          </div>
+          <div className={"notification error " + this.state.errorNotificationVisible}>
+            Oops! Something went wrong. 0_0
+          </div>
+        </div>
+        <div className="contact-form">
+          <form name="contactForm" id="contactForm" onSubmit={this.handleSubmit}>
+            <div className="form-field">
               <input type="text" name="_gotcha" style={{"display": "none"}} />
             </div>
-            <div class="form-field">
-              <input name="Name" type="text" id="contactName" placeholder="Your Name" required />
+            <div className="form-field">
+              <input type="text" placeholder="Your Name" required value={this.state.name} onChange={this.handleNameChange} />
             </div>
-            <div class="form-field">
-              <input name="_replyto" type="email" id="contactEmail" placeholder="Your Email ID" required />
+            <div className="form-field">
+              <input type="email" placeholder="Your Email ID" required value={this.state.email} onChange={this.handleEmailChange} />
             </div>
-            <div class="form-field">
-              <input name="_subject" type="text" id="contactSubject" placeholder="Subject" required />
+            <div className="form-field">
+              <input type="text" placeholder="Subject" required value={this.state.subject} onChange={this.handleSubjectChange} />
             </div>
-            <div class="form-field">
-              <textarea name="Message" id="contactMessage" placeholder="Message" rows="10" required></textarea>
+            <div className="form-field">
+              <textarea placeholder="Message" rows="10" required value={this.state.message} onChange={this.handleMessageChange} />
             </div>
-            <div class="form-field">
-              <input type="submit" id="contactFormSubmit" class="submitform" value="Send Message"></input>
+            <div className="form-field">
+              <input type="submit" className="submitform" value="Send Message"></input>
             </div>
           </form>
         </div>
-        <div id="contact-form-messages"></div>
       </div>
     )
   }

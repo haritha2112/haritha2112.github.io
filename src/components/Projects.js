a89f690c03de6923d4c7a9f39f1a025ae1f83489
haritha2112 <hrathin@g.clemson.edu>
2018-02-18 01:56:19 -0500

    Add content to project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,7 @@ import netflixImg from "../images/netflix-logo.png";
 import metubeImg from "../images/metube.jpg";
 import personalPortfolioImg from "../images/personal-portfolio.jpg";
 import myClemsonImg from "../images/myclemson-logo.png";
-import memoryGameImg from "../images/memory-game.jpg";
+import memoryGameImg from "../images/memory-game.png";
 import flashcardsImg from "../images/flashcards.jpg";
 
 export default class Projects extends Component {
@@ -16,41 +16,39 @@ export default class Projects extends Component {
         false,
         false,
         false,
-        false,
-        false,
         false
       ],
       projects: [
         {
           name: "Netflix Simulator",
-          description: "A web application that simulates Netflix experience by allowing users to watch TV show trailers. Includes features such as Search and Rating",
+          description: "A Netflix like application for TV show trailers. It allows users to search for TV shows, view and rate them.",
           techStack: "HTML5 | CSS3 | Sass | React.js",
-          backgroungImage: "linear-gradient(rgba(139, 0, 0, 0.5), rgba(139, 0, 0, 0.5)), url("+netflixImg+")"
+          backgroungImage: "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("+netflixImg+")"
         }, {
           name: "MeTube",
-          description: "A dynamic multimedia web application that mimics primary YouTube features. Includes features such as Multimedia file Upload/Download/View/Rate/Comment/Add to favorites; User account creation; Send/Receive messages; Add contacts; Group Discussions.",
+          description: "A dynamic multimedia web application that mimics primary YouTube features. It includes features such as multimedia file upload, download, view, rate, comment, favorite, user account creation, chat, add contacts and group discussions.",
           techStack: "LAMP Stack | HTML5 | Bootstrap",
-          backgroungImage: "linear-gradient(rgba(72, 60, 50, 0.5), rgba(72, 60, 50, 0.5)), url("+metubeImg+")"
+          backgroungImage: "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("+metubeImg+")"
         }, {
           name: "Personal Portfolio",
           description: "Designed this personal portfolio website to showcase my work-experience and skills.",
           techStack: "HTML5 | CSS3 | SVG | React.js | Sass",
-          backgroungImage: "linear-gradient(rgba(225, 98, 0, 0.5), rgba(225, 98, 0, 0.5)), url("+personalPortfolioImg+")"
+          backgroungImage: "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("+personalPortfolioImg+")"
         }, {
           name: "MyClemson App",
-          description: "Performed usability studies on Clemson University’s “MyClemson” mobile application. Designed a prototype using the results of the study which, based on a survey of 120 people, will increase the UX quality from 76% to 83%.",
+          description: "Performed usability studies on Clemson University’s MyClemson mobile application. Designed a prototype using the results of the study which, based on a survey of 120 people, will improve the user experience on the app.",
           techStack: "Human Centered Computing | Adobe InDesign",
-          backgroungImage: "linear-gradient(rgba(131, 131, 131, 0.5), rgba(131, 131, 131, 0.45)), url("+myClemsonImg+")"
+          backgroungImage: "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("+myClemsonImg+")"
         }, {
           name: "Memory Game",
-          description: "Currently working on building a browser-based card matching game.",
+          description: "A browser-based card matching game similar to Pairs - the classic playing card game.",
           techStack: "HTML5 | CSS3 | JavaScript",
-          backgroungImage: "linear-gradient(rgba(0, 0, 114, 0.5), rgba(0, 0, 114, 0.5)), url("+memoryGameImg+")"
+          backgroungImage: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("+memoryGameImg+")"
         }, {
           name: "Flashcards",
-          description: "A web application for vocabulary flashcards",
+          description: "A web application for vocabulary flashcards. Allows users to create cards, organize them into decks, and run practice sessions.",
           techStack: "Django | HTML5 | CSS3 | JavaScript",
-          backgroungImage: "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("+flashcardsImg+")"
+          backgroungImage: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("+flashcardsImg+")"
         }
       ]
     }
@@ -59,7 +57,7 @@ export default class Projects extends Component {
   }
 
   toggleClick(projectIndex) {
-    let nextClickStatus = [false, false, false, false, false, false, false, false];
+    let nextClickStatus = [false, false, false, false, false, false];
     nextClickStatus[projectIndex] = !this.state.isProjectClicked[projectIndex];
     this.setState({
       isProjectClicked: nextClickStatus

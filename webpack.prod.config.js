09507995eb6380260638c38ee2e6ee86ab336cae
haritha2112 <hrathin@g.clemson.edu>
2018-02-18 04:21:01 -0500

    Add seperate webpack config for prod

---
+++ webpack.prod.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    './src/index.js'
+  ],
+  module: {
+    rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: 'css-loader',
+          options: { importLoaders: 1 }
+        }, {
+          loader: "postcss-loader"
+        }, {
+          loader: "sass-loader"
+        }]
+      },
+      {
+        test: /\.(pdf|jpg|png|svg)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[path][name].[hash].[ext]",
+          },
+        },
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  }
+};

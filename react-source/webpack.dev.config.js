cadc28a12a338b1719158558b934c48248fe1347
haritha2112 <hrathin@g.clemson.edu>
2018-02-18 04:48:50 -0500

    Restructure project to work with github pages

--- react-source/webpack.dev.config.js
+++ react-source/webpack.dev.config.js
@@ -39,7 +39,7 @@ module.exports = {
     extensions: ['*', '.js', '.jsx']
   },
   output: {
-    path: __dirname + '/dist',
+    path: __dirname + '/..',
     publicPath: '/',
     filename: 'bundle.js'
   },
@@ -47,7 +47,7 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin()
   ],
   devServer: {
-    contentBase: './dist',
+    contentBase: './..',
     historyApiFallback: true,
     hot: true
   }
